00e9d61fe0a06677b5a9451810c2ab6232400511
mingshi2333 <1448513978@qq.com>
2023-07-09 17:33:41 +0800

    fixed

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,12 @@
 add_rules("mode.debug", "mode.release")
-set_languages("c++17")
+set_languages("c++20")
 add_toolchains("clang")
 add_requires("glfw", "glm","vulkan")
 
 target("vulkantest")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_files("src/*.hpp")
     add_packages("glfw", "glm","vulkan")
     add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
     if is_mode("release") then 
